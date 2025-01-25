3d2936c5eb2e2d8782d0ce2f3ef0a9d71ecfd1eb
GraemeIBB <vooner90@gmail.com>
2025-01-24 19:49:20 -0800

    object oriented!

--- Assets/scripts/Movement.cs
+++ Assets/scripts/Movement.cs
@@ -2,16 +2,19 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private Entity player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public float speed = 5.0f;
+    public float speed;
     void Start()
     {
-        
+        player = GetComponent<Entity>();
+        speed = player.speed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        speed = player.speed;
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);

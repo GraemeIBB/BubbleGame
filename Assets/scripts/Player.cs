d6d8e04de8df40caa7f0017b0ab9ae57871f9174
ErykHalicki <erykhalicki0@gmail.com>
2025-01-25 20:47:57 -0800

    skies and dying

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -1,19 +1,45 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : Entity
 {
-    
-    void start(){
-        base.Start();
-    }
+    public Image blackOverlay; // Assign your black UI Image in the inspector
+    public float fadeSpeed = 1f; // Adjust the speed of the fade
+    public AudioSource failSound;
+    public AudioSource mainSound;
 
+    private bool isFading = false;
+    Vector3 initialPos;
+
+    void Start(){
+        base.Start();
+        initialPos = transform.position;
+    }
 
     void Update(){
         base.Update();
         if(Input.GetKeyDown(KeyCode.Space)){
             base.jumpCommand = true;
         }
-    }
-    
+        if(transform.position.y < -30.0f && isFading == false){
+            isFading = true;
+            failSound.Play();
+            mainSound.Stop();
+        }
 
+        if (isFading){
+            Color currentColor = blackOverlay.color;
+            currentColor.a = Mathf.MoveTowards(currentColor.a, 1f, fadeSpeed * Time.deltaTime);
+            blackOverlay.color = currentColor;
+
+            if (currentColor.a >= 1f){
+                rb.position = initialPos;
+                currentColor.a = 0;
+                blackOverlay.color = currentColor;
+                rb.AddForce(rb.GetAccumulatedForce() * -1f, ForceMode.Force);
+                isFading = false; 
+                mainSound.Play();
+            }
+        }
+    }
 }
